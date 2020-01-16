05e51fbe68551cb24d09e3b925f2937d4be2d5bb
黎伟航 <lwh1094464365@163.com>
2020-01-16 18:28:18 +0800

    Initial Commit

---
+++ pages/grade/grade.js
@@ -0,0 +1,137 @@
+// pages/grade/grade.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    getUrl: '', // 请求URL
+    token: '', // 请求令牌
+    xh: '', // 请求学号
+    stu_info: '',// 学生信息
+    resArray: [], // 请求到的成绩信息
+
+    semester: ['2019-2020-1', '2018-2019-2', '2018-2019-1', '2017-2018-2', '2017-2018-1'],
+    index: 0,
+
+  },
+
+  // 选择学期
+  sectionSemester: function(e) {
+    this.setData({
+      index: e.detail.value
+    })
+
+    // 改变请求URL中的学期
+    this.setData({
+      getUrl: 'http://jw.nnxy.cn/app.do?method=getCjcx&xh=' + this.data.xh + '&xnxqid=' + this.data.semester[this.data.index]
+    })
+
+    var _this = this
+    // 重新请求
+    wx.request({
+      url: _this.data.getUrl, // 获取成绩请求
+      method: 'get',
+      header: {
+        'content-type': 'application/json', // 默认值
+        'token': _this.data.token // 登录令牌
+      },
+      success(res) {
+        console.log(res.data.result)
+        _this.setData({
+          resArray: res.data.result
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var _this = this
+
+    // 获取登录令牌
+    _this.setData({
+      token: wx.getStorageSync('token')
+    })
+
+    // 获取登录学号
+    _this.setData({
+      xh: wx.getStorageSync('xh')
+    })
+
+    // 获取学生信息
+    _this.setData({
+      stu_info: wx.getStorageSync('stu_info')
+    })
+
+    _this.setData({
+      getUrl: 'http://jw.nnxy.cn/app.do?method=getCjcx&xh=' + _this.data.xh + '&xnxqid=' + _this.data.semester[_this.data.index]
+    })
+
+    // 网络请求
+    wx.request({
+      url: _this.data.getUrl, // 获取成绩请求
+      header: {
+        'content-type': 'application/json', // 默认值
+        'token': _this.data.token // 登录令牌
+      },
+      success(res) {
+        console.log(res.data.result)
+        _this.setData({
+          resArray: res.data.result
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,149 @@
+// pages/login/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    xh: '',
+    pwd: '',
+    getUrl: ''
+  },
+
+  // 获取学号
+  xhinput: function(e) {
+    this.setData({
+      xh: e.detail.value
+    })
+  },
+
+  // 获取密码
+  pwdinput: function(e) {
+    this.setData({
+      pwd: e.detail.value
+    })
+  },
+
+  // 登录
+  login: function() {
+    console.log("学号:" + this.data.xh)
+    console.log("密码:" + this.data.pwd)
+    var _this = this
+    this.setData({
+      getUrl: 'http://jw.nnxy.cn/app.do?method=authUser&xh=' + this.data.xh + '&pwd=' + this.data.pwd
+    })
+    console.log(this.data.getUrl)
+
+    // 网络请求
+    wx.request({
+      url: this.data.getUrl, // 向南宁学院教务系统请求token
+      success(res) {
+        console.log(res.data.token)
+        if (-1 == res.data.token) {
+          wx.showModal({
+            title: '提示',
+            content: '登录失败，请检查学号和密码',
+            showCancel: false,
+            success(res) {
+              _this.setData({
+                xh: '',
+                pwd: ''
+              })
+            }
+          })
+          return;
+        } else {
+          // 获取token和保存本地缓存
+          wx.setStorage({
+            key: "token",
+            data: res.data.token
+          })
+          // 保存学号到本地缓存
+          wx.setStorage({
+            key: "xh",
+            data: _this.data.xh
+          })
+          // 保存学生信息到本地缓存
+          wx.setStorage({
+            key: 'stu_info',
+            data: res.data.user.userdwmc + ' ' + res.data.user.useraccount + res.data.user.username,
+          })
+
+          // 提示及跳转
+          wx.redirectTo({
+            url: '../grade/grade',
+          })
+          wx.showToast({
+            title: '登录成功',
+            icon: 'success',
+            duration: 2000
+          })
+        }
+      }
+    })
+  },
+
+  linkShow() {
+    wx.showModal({
+      title: '提示',
+      content: '并没有这些功能，先别乱点',
+      showCancel: false
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
